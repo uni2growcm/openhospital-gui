478b9dbcac3accc679a8a50f64dd1e6ee08559f3
BILL <tatemsabill@gmail.com>
2024-12-05 18:05:55 +0100

    Update MainMenu file

--- src/main/java/org/isf/menu/gui/MainMenu.java
+++ src/main/java/org/isf/menu/gui/MainMenu.java
@@ -331,7 +331,7 @@ public class MainMenu extends JFrame implements ActionListener, LoginListener, C
 		// remove mortuary if not enabled
 		flag_Mortuary = GeneralData.ENABLEMORTUARYMODULE;
 		if(!flag_Mortuary){
-			List<UserMenuItem> junkMenu = myMenu.stream().filter(item -> "mortuary".equalsIgnoreCase(item.getCode())).toList();
+			List<UserMenuItem> junkMenu = myMenu.stream().filter(item -> "mortuarystays".equalsIgnoreCase(item.getCode())).toList();
 			myMenu.removeAll(junkMenu);
 		}
 
