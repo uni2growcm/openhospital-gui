d0a24c85305f6bd71f248ae15b258151bc82ff0b
Tatemsa <tatemsabill@gmail.com>
2024-12-12 10:01:18 +0100

    Change a value of a page size global variable

--- src/main/java/org/isf/medicalstock/gui/MovStockBrowser.java
+++ src/main/java/org/isf/medicalstock/gui/MovStockBrowser.java
@@ -150,7 +150,7 @@ public class MovStockBrowser extends ModalJFrame {
 	private int pages = 0;
 	private int currentPage = 0;
 	private long totalMovements = 0;
-	private final int PAGE_SIZE = 10;
+	private final int PAGE_SIZE = 100;
 	private BigDecimal totalAmount;
 	private MovBrowserModel model;
 	private List<Movement> allMoves;
