c27480b92b694573a97266570d5a68de3a70eb67
lenatoukam12 <106070760+lenatoukam12@users.noreply.github.com>
2025-01-24 13:32:31 +0100

    Update src/main/java/org/isf/accounting/gui/PatientBillEdit.java
    Co-authored-by: FOFOU FONZAM Gui Arnaud <gui.fofou@uni2growcameroun.com>

--- src/main/java/org/isf/accounting/gui/PatientBillEdit.java
+++ src/main/java/org/isf/accounting/gui/PatientBillEdit.java
@@ -776,7 +776,7 @@ public class PatientBillEdit extends JDialog implements SelectionListener {
 		return jLabelWard;
 	}
 
-	private JLabel getJTextWard() {
+	private JLabel getJLabelWardDescription() {
 		if (jTextWard == null) {
 			jTextWard = new JLabel();
 			jTextWard.setPreferredSize(WARD_DIMENSION);
