7ea55a01975d0ae61f6f284efe2c8e6737c836a9
ArnaudFofou <gui.fofou@umi2growcameroun.com>
2025-02-07 11:16:15 +0100

    fix error

--- src/test/java/org/isf/lab/gui/elements/ExamComboBoxTest.java
+++ src/test/java/org/isf/lab/gui/elements/ExamComboBoxTest.java
@@ -28,6 +28,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.isf.exa.model.Exam;
+import org.isf.exa.model.ExamTarget;
 import org.isf.lab.model.Laboratory;
 import org.junit.jupiter.api.Test;
 
@@ -37,7 +38,9 @@ class ExamComboBoxTest {
 	void shouldCreateComboBoxWithPersistedExamsAndExamFromLaboratorySelected() {
 		// given:
 		Exam exam = TestExam.examWithCode("test");
+		exam.setTarget(ExamTarget.no);
 		Exam exam2 = TestExam.examWithCode("test2");
+		exam2.setTarget(ExamTarget.no);
 		List<Exam> exams = Arrays.asList(exam, exam2);
 		Laboratory laboratory = new Laboratory();
 		laboratory.setExam(exam2);
