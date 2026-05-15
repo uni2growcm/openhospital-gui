4b6da9badb8700f035bf0e8759c9f53c5a8b4544
rivald123 <stephannintedem@gmail.com>
2026-05-15 15:39:46 +0100

    fix import

--- src/main/java/org/isf/admission/gui/AdmittedPatientBrowser.java
+++ src/main/java/org/isf/admission/gui/AdmittedPatientBrowser.java
@@ -21,7 +21,17 @@
  */
 package org.isf.admission.gui;
 
-import java.awt.*;
+
+import java.awt.AWTEvent;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
