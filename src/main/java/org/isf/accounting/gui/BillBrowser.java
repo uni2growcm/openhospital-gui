a6b4936b3ae898a97e3bb2b3c763276847e717b3
ndong.phanuel@uni2growcameroun.com <hemaserenankemenyi@gmail.com>
2026-06-03 15:10:17 +0100

    add logic

--- src/main/java/org/isf/accounting/gui/BillBrowser.java
+++ src/main/java/org/isf/accounting/gui/BillBrowser.java
@@ -1355,6 +1355,7 @@ public class BillBrowser extends ModalJFrame implements PatientBillListener {
 				options = new ArrayList<>();
 				options.add(MessageBundle.getMessage("angal.billbrowser.shortreportonlybaddebt.txt"));
 				options.add(MessageBundle.getMessage("angal.billbrowser.fullreportallbills.txt"));
+				options.add(MessageBundle.getMessage("angal.billbrowser.allincomesgroupbyitemcategories.txt"));
 
 				icon = new ImageIcon("rsc/icons/list_dialog.png");
 				option = (String) MessageDialog.inputDialog(this,
@@ -1373,6 +1374,10 @@ public class BillBrowser extends ModalJFrame implements PatientBillListener {
 				if (options.indexOf(option) == 1) {
 					new GenericReportFromDateToDate(from, to, "rpt_stat", GeneralData.BILLSREPORT,
 							MessageBundle.getMessage("angal.billbrowser.fullreportallbills.txt"), false);
+				}
+				if (options.indexOf(option) == 2) {
+					new GenericReportFromDateToDate(from, to, "rpt_base", "OH004_01_AllIncomesGroupByItemCategories",
+							MessageBundle.getMessage("angal.billbrowser.allincomesgroupbyitemcategories.txt"), false);
 				}
 			});
 		}
